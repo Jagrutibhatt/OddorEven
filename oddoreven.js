496f66d03e970ec7707dcbbc34b5055747081364
Jagrutibhatt <109428301+Jagrutibhatt@users.noreply.github.com>
2022-07-17 22:21:17 +0530

    Add files via upload

---
+++ oddoreven.js
@@ -0,0 +1,14 @@
+
+function oddOrEven (num) {
+    if (num%2==0){
+        console.log("This number is Even")
+    }
+    else {
+        console.log("This number is odd")
+    }
+}
+
+oddOrEven(2)
+oddOrEven(3)
+oddOrEven(7)
+oddOrEven(10)
